726611d54a02af581aa42f444f02fa4d5c398e4a
thisdjango <tsarkova.92@mail.ru>
2019-11-11 19:13:27 +0300

    lol

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("thisdjango was here!!!");
-        //yhftgyfffuyfty
+        //hah here we go again
     }
 }
