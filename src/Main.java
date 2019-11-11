dd34e49fe2736ad7bd2d586c7dcc8fece4f5b785
Qwerrty <ilyinsky.igor2010@yandex.ru>
2019-11-11 19:02:14 +0300

    3 изменения

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
+        int i=0;
+        int k=i/2;
 
-        System.out.println("45zz");
+        System.out.println("456zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
 
     }
 }
