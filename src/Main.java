b95ee27f9347a504a1e13be5aad81fdbdf9560b9
ImSeDmi <seniordmitriy@mail.ru>
2019-11-11 19:00:52 +0300

    + 2 переменные + вывод

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("45zz");
-
+        int i = 0;
+        int j = 0;
+        System.out.println("lol"+i+j);
     }
 }
