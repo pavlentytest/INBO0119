85ad26fc0a6619f4d83c984e50e54adfdcc5163e
NASTYAKATYA <katyabi2010@gmail.com>
2019-11-11 19:04:39 +0300

    Merge branch 'master' of C:\IN with conflicts.

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("thisdjango was here!!!");
-	System.out.println("Hah, I got it");
-	System.out.println("You are so slow(((");
+        System.out.println("git git");
+
     }
 }
