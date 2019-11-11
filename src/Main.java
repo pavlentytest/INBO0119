33f884c4a9eb001dd1d47cef7222e0509d5e50f6
Konstantin <k.azarov@inbox.ru>
2019-11-11 19:11:35 +0300

    yep

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        int i = 0;
-        int j = 0;
-        System.out.println("lol"+i+j);
 
         System.out.println("45zz");
         System.out.println("keklol");
